a30acfe6c7fa332d9d49b9f962b77434fb5fe208
acparkgh <acpark123@gmail.com>
2021-04-28 15:32:55 -0400

    array destructuring

--- starter/script.js
+++ starter/script.js
@@ -11,6 +11,11 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return (
+      [ this.starterMenu[starterIndex], this.mainMenu[mainIndex] ]
+    );
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +32,37 @@ const restaurant = {
     },
   },
 };
+
+console.log(restaurant.categories);
+
+// cat1 = 'Italian'
+// cat2 = 'Pizzeria'
+
+let [cat1, cat2] = restaurant.categories;
+console.log("cat1:", cat1);
+console.log("cat2:", cat2);
+
+[cat1, cat2] = [cat2, cat1];
+console.log("cat1:", cat1);
+console.log("cat2:", cat2);
+
+console.log(restaurant.categories);
+
+console.log(restaurant.order(2, 0));
+
+const [starter, mainMeal] = restaurant.order(2, 0);
+console.log("Starter: ", starter, "&", "Main: ", mainMeal);
+
+const nestedArray = [2, 4, [5, 6]];
+const [i, , [k, l]] = nestedArray;
+console.log(i, k, l);
+
+const [p = 1, q = 1, r = 1, s = 1] = [, 9, 8];
+console.log(p, q, r, s);
+
+const array1 = [, 1, 2, , , 4, , , ,]
+console.log(array1);
+
+const [e1, f1, g1, h1, i1, j1, k1, l1, m1, n1] = array1;
+console.log(e1, f1, g1, h1, i1, j1, k1, l1, m1, n1);
+
