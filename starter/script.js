c7065b664e32fbad8c0f9b60b3b394993a216edf
acparkgh <acpark123@gmail.com>
2021-05-01 00:12:24 -0400

    Enhanced Object Literals

--- starter/script.js
+++ starter/script.js
@@ -4,24 +4,15 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  restaurantName: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
-    return (
-      [ this.starterMenu[starterIndex], this.mainMenu[mainIndex] ]
-    );
-  },
-  openingHours: {
-    thu: {
+const weekdays = [ "mon", "tue", "wed", "thu", "fri" ] 
+const openingHours = {
+    // thu: {
+    [weekdays[3]]: {
       open: 12,
       close: 22,
     },
-    fri: {
+    // fri: {
+    [weekdays[4]]: {
       open: 11,
       close: 23,
     },
@@ -29,18 +20,33 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
+};
+
+// Data needed for first part of the section
+const restaurant = {
+  restaurantName: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // openingHours: openingHours,
+  openingHours,
+  order (starterIndex, mainIndex) {
+    return (
+      [ this.starterMenu[starterIndex], this.mainMenu[mainIndex] ]
+    );
   },
   // orderDelivery: function (obj) {
   //   console.log(obj);
   // }
-  orderDelivery: function ({ mainIndex,
-                             starterIndex,
-                             address: deliveryAddress,
-                             time: deliveryTime }) {
+  orderDelivery ({ mainIndex,
+                   starterIndex,
+                   address: deliveryAddress,
+                   time: deliveryTime }) {
     // console.log(this);
     console.log(`Order Received: ${this.starterMenu[mainIndex]} and ${this.mainMenu[starterIndex]} will be delivered to ${deliveryAddress} at ${deliveryTime}.`)
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta (ing1, ing2, ing3) {
     console.log(`Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
 
@@ -72,14 +78,14 @@ restaurant.orderDelivery({
   starterIndex: 2,
 })
 
-const { restaurantName, openingHours, categories } = restaurant;
-console.log(restaurantName, categories, openingHours);
+// const { restaurantName, openingHours, categories } = restaurant;
+// console.log(restaurantName, categories, openingHours);
 
 const { restaurantName: restName, openingHours: hours, categories: tags } = restaurant;
 console.log(restName, tags, hours);
 
-const { fri: {open: openHours, close} } = openingHours;
-console.log(openHours, close);
+// const { fri: {open: openHours, close} } = openingHours;
+// console.log(openHours, close);
 
 const { desserts = [], starterMenu: appertizer = [] } = restaurant;
 console.log(desserts, appertizer);
@@ -173,9 +179,6 @@ restaurant.orderPizza("BBQ Chicked", "mushroom", "green pepper", "onion");
 
 
 console.log(`---------- Rest Operator -----------------------`);
-
-
-
 
 console.log(restaurant.categories);
 
