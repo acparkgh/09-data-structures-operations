3862c042c8323f4d136a8993a037601e4b7712a7
acparkgh <acpark123@gmail.com>
2021-05-03 00:35:00 -0400

    blog hoisting

--- starter/script.js
+++ starter/script.js
@@ -1,44 +1,37 @@
 "use strict";
 
-const airline = "TAP Air Portugal";
-const plane = "A320";
+fullName("Andrew", "Park");
 
-console.log(plane[0]);
-console.log(plane[1]);
-console.log(plane[2]);
-console.log(plane[3]);
-console.log("B737"[2]);
-
-console.log(airline.length);
-console.log("B777".length);
-
-console.log(airline.indexOf("r"))
-console.log(airline.lastIndexOf("r"));
-console.log(airline.indexOf("Air"));
-
-console.log(airline.slice(4));
-console.log(airline.slice(4, 7));
-console.log(airline.slice(0, airline.indexOf(" ")));
-console.log(airline.slice(airline.lastIndexOf("P")));
-console.log(airline.slice(airline.lastIndexOf(" ") + 1));
-
-console.log(airline.slice(-8));
-console.log(airline.slice(1, -1));
-
-
-const checkMiddleSeat = function (seat) {
-  const seatPosition = seat.slice(-1);
-  const middleSeat = "You got middle seat 🥺";
-  const isleSeat = "You got lucky!!!"
-  return (
-    seatPosition === "B" || seatPosition === "E" ? middleSeat : isleSeat
-  );
+function fullName (firstName, lastName) {
+  const fullName = `${firstName} ${lastName}`;
+  console.log(fullName);
 };
 
-console.log(checkMiddleSeat("11B"));
-console.log(checkMiddleSeat("11E"));
-console.log(checkMiddleSeat("11A"));
 
-console.log(new String("Jonas"));
-console.log(typeof new String("Jonas"));
-console.log(typeof new String("Jonas").slice(-1));
+
+
+
+
+
+
+
+
+
+// const firstName = "Andrew";
+// const lastName = "Park";
+
+
+
+
+
+
+
+
+
+
+// const lastName = "Park";
+// const fullName = firstName + middleInitial + lastName
+// console.log(`${firstName} is ${age}`);
+// // let age = 10;
+
+// console.log(firstName, lastName);
