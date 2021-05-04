45faf0f4a89377f08f70bc907f5328f19a77d23c
acparkgh <acpark123@gmail.com>
2021-05-04 00:10:31 -0400

    String method practice

--- starter/script.js
+++ starter/script.js
@@ -145,3 +145,20 @@ const convertText = function () {
   
 document.querySelector("button").addEventListener("click", convertText);
 
+console.log("________________________________________________________________");
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const flightsSplit = flights.split("+");
+const airportCode = function (message) {
+  return (
+    message.toUpperCase().slice(0, 3)
+  )
+}
+
+for (const flight of flightsSplit) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${ type.startsWith("_Delayed") ? "😢" : "" }${ type.replace(/_/g, "") } from ${ airportCode(from) } to ${ airportCode(to) } (${ time.replace(":", "h") })`.padStart(42);
+  console.log(output);
+};
